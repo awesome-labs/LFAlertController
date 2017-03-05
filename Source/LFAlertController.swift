b92388715453fa42543d3bf738482c1df32b609d
Lucas Farah <lucas.farah@me.com>
2017-03-05 19:16:48 -0300

    Fixed hardcoded UIAlertActionStyle

--- Source/LFAlertController.swift
+++ Source/LFAlertController.swift
@@ -8,81 +8,81 @@
 import UIKit
 
 public extension UIAlertController {
-  
-  /// EZSE: easy way to present UIAlertController
-  public func show(){
-    UIApplication.shared.keyWindow?.rootViewController?.present(self, animated: true, completion: nil)
-  }
-  
-  /// LFAlertController: Simple UIAlertController with actions and handler
-  @nonobjc public convenience init(title: String, message: String, preferredStyle: UIAlertControllerStyle, actions: [UIAlertAction], handler: @escaping (_ action:UIAlertAction,_ index:Int) -> ())
-  {
-    self.init(title: title, message: message, preferredStyle: preferredStyle)
     
-    for action in actions
-    {
-      let act = UIAlertAction(title: action.title!, style: action.style, controller: self)
-      self.addAction(act)
+    /// EZSE: easy way to present UIAlertController
+    public func show(){
+        UIApplication.shared.keyWindow?.rootViewController?.present(self, animated: true, completion: nil)
     }
     
-    NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "NotificationIdentifier"), object: nil, queue: nil) { (notification) -> Void in
-      
-      if let index = notification.object
-      {
-        handler(self.actions[index as! Int], index as! Int)
-      }
+    /// LFAlertController: Simple UIAlertController with actions and handler
+    @nonobjc public convenience init(title: String, message: String, preferredStyle: UIAlertControllerStyle, actions: [UIAlertAction], handler: @escaping (_ action:UIAlertAction,_ index:Int) -> ())
+    {
+        self.init(title: title, message: message, preferredStyle: preferredStyle)
+        
+        for action in actions
+        {
+            let act = UIAlertAction(title: action.title!, style: action.style, controller: self)
+            self.addAction(act)
+        }
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "NotificationIdentifier"), object: nil, queue: nil) { (notification) -> Void in
+            
+            if let index = notification.object
+            {
+                handler(self.actions[index as! Int], index as! Int)
+            }
+        }
     }
-  }
-  
-  /// LFAlertController: Simple UIAlertController with actions
-  @nonobjc public convenience init(title: String, message: String, preferredStyle: UIAlertControllerStyle, actions: [UIAlertAction])
-  {
-    self.init(title: title, message: message, preferredStyle: preferredStyle)
     
-    for action in actions
+    /// LFAlertController: Simple UIAlertController with actions
+    @nonobjc public convenience init(title: String, message: String, preferredStyle: UIAlertControllerStyle, actions: [UIAlertAction])
     {
-      self.addAction(action)
+        self.init(title: title, message: message, preferredStyle: preferredStyle)
+        
+        for action in actions
+        {
+            self.addAction(action)
+        }
     }
-  }
-  
-  //EZSE: Returns the index of UIAlertViewController's action from title
-  public func indexOfActionForTitle(title:String) -> Int
-  {
-    for action in self.actions
+    
+    //EZSE: Returns the index of UIAlertViewController's action from title
+    public func indexOfActionForTitle(title:String) -> Int
     {
-      if action.title == title
-      {
-        return self.actions.index(of: action)!
-      }
+        for action in self.actions
+        {
+            if action.title == title
+            {
+                return self.actions.index(of: action)!
+            }
+        }
+        return 99
     }
-    return 99
-  }
-  
-  //LFAlertController: Appends UIAlertAction with NSNotification
-  public func appendAction(action:UIAlertAction)
-  {
-    let act = UIAlertAction(title: action.title!, style: action.style, controller: self)
-    self.addAction(act)
-  }
-  
+    
+    //LFAlertController: Appends UIAlertAction with NSNotification
+    public func appendAction(action:UIAlertAction)
+    {
+        let act = UIAlertAction(title: action.title!, style: action.style, controller: self)
+        self.addAction(act)
+    }
+    
 }
 
 extension UIAlertAction {
-  
-  /// LFAlertController: UIAlertAction initialization without handler
-  public convenience init(title: String,style: UIAlertActionStyle)
-  {
-    self.init(title: title, style: .default) { (action) -> Void in
+    
+    /// LFAlertController: UIAlertAction initialization without handler
+    public convenience init(title: String,style: UIAlertActionStyle)
+    {
+        self.init(title: title, style: style) { (action) -> Void in
+        }
     }
-  }
-  
-  /// LFAlertController: private UIAlertAction initialization with controller
-  internal convenience init(title: String,style: UIAlertActionStyle,controller:UIAlertController) {
-    self.init(title: title, style: .default) { (action) -> Void in
-        
-        
-    let notification = Notification(name: Notification.Name(rawValue: "NotificationIdentifier"), object: controller.indexOfActionForTitle(title: title))
-      NotificationCenter.default.post(notification)
+    
+    /// LFAlertController: private UIAlertAction initialization with controller
+    internal convenience init(title: String,style: UIAlertActionStyle,controller:UIAlertController) {
+        self.init(title: title, style: style) { (action) -> Void in
+            
+            
+            let notification = Notification(name: Notification.Name(rawValue: "NotificationIdentifier"), object: controller.indexOfActionForTitle(title: title))
+            NotificationCenter.default.post(notification)
+        }
     }
-  }
 }
